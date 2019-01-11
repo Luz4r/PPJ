11731eec57f9b8536a891bb0b2df9a6a932ec0af
Rafał Piórek <s18660@pjwstk.edu.pl>
2019-01-11 15:19:56 +0100

    Exerciese 4 in Zestaw24

--- IDEA/IntelliJ/src/Zestaw24/Main.java
+++ IDEA/IntelliJ/src/Zestaw24/Main.java
@@ -13,13 +13,45 @@ public class Main {
             for(int i : primeNumbers)
                 fileWriter.write(Integer.toString(i) + " ");
 
-            fileWriter.flush();
             fileWriter.close();
         }catch(IOException e){
             e.printStackTrace();
         }*/
 
-        System.out.println(check("file.txt"));
+        //System.out.println(check("file.txt"));
+
+        try{
+            FileInputStream fileInputStream = new FileInputStream("example.txt");
+            int tmp;
+            int i = 0;
+            char[] array = new char[26];
+            char[] checkedChars = new char[array.length];
+
+            while((tmp = fileInputStream.read()) != -1){
+                array[i++] = (char)tmp;
+            }
+
+            for(int j = 0; j < array.length; j++){
+                for(int k = 0; k < checkedChars.length; k++){
+
+
+                }
+                for(int l = j; l < array.length; l++){
+
+                }
+            }
+
+            fileInputStream.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
+        /*try {
+            createFiles(3);
+        }catch(IOException e){
+            e.printStackTrace();
+        }*/
 
     }
 
@@ -50,11 +82,11 @@ public class Main {
 
     public static boolean check(String filePath){
         try {
-            FileInputStream fileInputStream = new FileInputStream(filePath);
+            FileReader fileReader = new FileReader(filePath);
 
             int tmp;
             StringBuilder sb = new StringBuilder();
-            while((tmp = fileInputStream.read()) != -1){
+            while((tmp = fileReader.read()) != -1){
                 sb.append((char)tmp);
             }
 
@@ -68,5 +100,35 @@ public class Main {
             e.printStackTrace();
         }
         return true;
+    }
+
+    public static void createFiles(int howMany) throws IOException{
+        int[] randomNumbers = new int[10];
+
+        for(int i = 0; i <= howMany; i++) {
+            for (int j = 0; j < randomNumbers.length; j++)
+                randomNumbers[j] = (int) (Math.random() * 10001);
+
+            for(int j = 0; j < randomNumbers.length - 1; j++){
+                int index = j;
+                for(int k = j + 1; k < randomNumbers.length; k++){
+                   if(randomNumbers[index] > randomNumbers[k]){
+                       index = k;
+                   }
+                }
+                int tmp = randomNumbers[index];
+                randomNumbers[index] = randomNumbers[j];
+                randomNumbers[j] = tmp;
+            }
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter("inputData" + i + ".txt"));
+
+            StringBuilder sb = new StringBuilder();
+            for (int k : randomNumbers)
+                sb.append(k).append(" ");
+
+            bw.write(sb.toString());
+            bw.close();
+        }
     }
 }
