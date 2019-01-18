11321c4823043f319f86cbd290f711909854cbaa
Rafał Piórek <s18660@pjwstk.edu.pl>
2019-01-18 15:28:46 +0100

    Add exercise 2 and 3 from Zestaw26

--- IDEA/IntelliJ/src/Zestaw26/Main.java
+++ IDEA/IntelliJ/src/Zestaw26/Main.java
@@ -1,12 +1,13 @@
 package Zestaw26;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
-        Color color = new Color(255, 125, 25);
+        //Color color = new Color(255, 125, 25);
 
-        System.out.println(color);
+        //System.out.println(color);
 
         Color[] colors = new Color[10000];
 
@@ -15,24 +16,38 @@ public class Main {
         }
 
         try{
-            //saveToTxt(colors);
-            saveToBinary(colors);
+            saveToTxt(colors);
+            saveToBinary(colors);       //Commented so as not to generate files each time program runs
+            readFromTxt();
+            readFromBinary();
         }catch(IOException e){
             e.printStackTrace();
         }
     }
 
     static void saveToTxt(Color[] colors) throws IOException {
+        long startTime = System.currentTimeMillis();
+
         BufferedWriter br = new BufferedWriter(new FileWriter("stringColors.txt"));
 
         for(Color color : colors){
-            br.write(color.toString() + "\n");
+            StringBuilder sb = new StringBuilder();
+
+            sb  .append(color.getValue() & 0b11111111).append(" ")
+                .append(color.getValue() >> 8 & 0b11111111).append(" ")
+                .append(color.getValue() >> 16 & 0b11111111).append("\n");
+            br.write(sb.toString());
+            //br.write(color.toString() + "\n");
         }
 
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
         br.close();
     }
 
     static void saveToBinary(Color[] colors) throws IOException{
+        long startTime = System.currentTimeMillis();
+
         FileOutputStream fos = new FileOutputStream("byteColors.rafal");
 
         for(Color color : colors){
@@ -41,6 +56,63 @@ public class Main {
             fos.write(color.getValue() >> 16 & 0b11111111);
         }
 
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
         fos.close();
+    }
+
+    static Color[] readFromTxt() throws IOException{
+        long startTime = System.currentTimeMillis();
+        Color[] colors = new Color[10000];
+        int i = 0;
+
+        BufferedReader br = new BufferedReader(new FileReader("stringColors.txt"));
+
+        String line =  br.readLine();
+        StringTokenizer st = new StringTokenizer(line);
+
+        while(st.hasMoreTokens()){
+            int red = Integer.parseInt(st.nextToken());
+            int green = Integer.parseInt(st.nextToken());
+            int blue = Integer.parseInt(st.nextToken());
+            colors[i++] = new Color(red, green, blue);
+
+            line = br.readLine();
+            if(line != null){
+                st = new StringTokenizer(line);
+            }
+        }
+
+        br.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+        return colors;
+    }
+
+    static Color[] readFromBinary() throws IOException{
+        long startTime = System.currentTimeMillis();
+
+        Color[] colors = new Color[10000];
+        int i = 0;
+
+        FileInputStream fis = new FileInputStream("byteColors.rafal");
+
+        int tmp = fis.read();
+
+        while(tmp != -1){
+            int[] rgb = new int[3];
+            for(int j = 0; j < 3; j++){
+                rgb[j] = tmp;
+                tmp = fis.read();
+            }
+            colors[i++] = new Color(rgb[0], rgb[1], rgb[2]);
+        }
+
+        fis.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+        return colors;
     }
 }
